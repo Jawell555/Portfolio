d492d5eaeb496ffdf992382be4aad5d3a5e1cbfe
Jawell555 <sanlorenzojruell@gmail.com>
2026-09-04 16:13:40 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const navigators = document.querySelectorAll(".content-selector");
+const sections = document.querySelectorAll(".page-content");
+
+navigators.forEach((navigator) => {
+  navigator.addEventListener("click", () => {
+    const targetId = navigator.getAttribute("data-target");
+
+    sections.forEach((section) => {section.classList.add("hidden");});
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+        targetSection.classList.remove("hidden");
+    }
+  });
+});
+
+const form = document.getElementById('form');
+const submitBtn = form.querySelector('button[type="submit"]');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    formData.append("access_key", "3c64211a-7c1b-4d17-a137-11f9ff4e3bcf");
+
+    const originalText = submitBtn.textContent;
+
+    submitBtn.textContent = "Sending...";
+    submitBtn.disabled = true;
+
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert("Success! Your message has been sent.");
+            form.reset();
+        } else {
+            alert("Error: " + data.message);
+        }
+
+    } catch (error) {
+        alert("Something went wrong. Please try again.");
+    } finally {
+        submitBtn.textContent = originalText;
+        submitBtn.disabled = false;
+    }
+});
